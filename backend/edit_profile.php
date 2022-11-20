1808c63fee1772617c5e5ce899ce020f54220817
tiahelou10 <tia.elhelou@outlook.com>
2022-11-20 19:38:29 +0200

    Backend: edited the edit profile php

--- backend/edit_profile.php
+++ backend/edit_profile.php
@@ -4,19 +4,28 @@ include('connect.php');
 
 $response = [];
 
-$id = $_POST['id'];
-$name = $_POST['name'];
-$email = $_POST['email'];
-$username = $_POST['username'];
-$password = $_POST['password'];
-$bio = $_POST['bio'];
+if (isset($_POST['name']) && isset($_POST['email']) && isset($_POST['username']) && isset($_POST['password'])) {
+
+    $id = $_POST['id'];
+    $name = $_POST['name'];
+    $email = $_POST['email'];
+    $username = $_POST['username'];
+    $password = $_POST['password'];
+    $bio = $_POST['bio'];
 
 
-$query = $mysqli->prepare("UPDATE users SET name = ?, email = ?, username = ?, password = ?, bio = ? WHERE id = ?;");
-$query->bind_param("sssssi", $name, $email, $username, $password, $bio, $id);
-$query->execute();
+    $query = $mysqli->prepare("UPDATE users SET name = ?, email = ?, username = ?, password = ?, bio = ? WHERE id = ?;");
+    $query->bind_param("sssssi", $name, $email, $username, $password, $bio, $id);
+    $query->execute();
 
-$response["success"] = true;
-echo json_encode($response);
+    $response["success"] = true;
+    echo json_encode($response);
+
+} else {
+
+    $response["success"] = false;
+    echo json_encode($response);
+
+}
 
 ?>
