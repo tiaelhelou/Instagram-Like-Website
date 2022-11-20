ae2c3e7eb871446838db99aa8a22b8f290e43659
tiahelou10 <tia.elhelou@outlook.com>
2022-11-20 18:35:39 +0200

    Backend: added the login php

---
+++ backend/login.php
@@ -0,0 +1,33 @@
+<?php
+
+include("connect.php");
+
+
+$username = $_POST['username'];
+$password = $_POST['password'];
+
+$query = $mysqli->prepare("SELECT id FROM users WHERE username = ? AND password = ?");
+$query->bind_param("ss", $username, $password);
+$query->execute();
+
+$response = [];
+
+$result = $query->get_result();
+$id = $result->fetch_assoc();
+$response[] = $id;
+
+if (mysqli_num_rows($result) > 0) {
+
+    $_SESSION['id'] = $id;
+
+    echo json_encode($response);
+
+} 
+else {
+    $response["success"] = false;
+
+    echo json_encode($response);
+
+}
+
+?>
