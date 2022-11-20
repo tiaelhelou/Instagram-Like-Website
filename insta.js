6f77f994511650de4fc2bca90c576534b7e4267f
tiahelou10 <tia.elhelou@outlook.com>
2022-11-20 23:27:55 +0200

    Frontend: added post api

--- insta.js
+++ insta.js
@@ -14,3 +14,19 @@ pages.getAPI = async (api_url) => {
         workshop_pages.Console("Error from Linking (GET)", error);
     }
 }
+
+pages.postAPI = async (api_url, api_data, api_token = null) => {
+    try {
+        return await axios.post(
+            api_url,
+            api_data,
+            {
+                headers: {
+                    'Authorization': "token" + api_token
+                }
+            }
+        );
+    } catch (error) {
+        workshop_pages.Console("Error from Linking (POST)", error);
+    }
+}
