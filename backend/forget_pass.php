6505846d5a2f8123c7f72c799ab0606d88116dd6
tiahelou10 <tia.elhelou@outlook.com>
2022-11-21 00:00:54 +0200

    Backend: added header in all php files

--- backend/forget_pass.php
+++ backend/forget_pass.php
@@ -1,4 +1,9 @@
 <?php
+
+header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Methods: GET, POST');
+header("Access-Control-Allow-Headers: X-Requested-With");
+
 include("connect.php");
 
 if (isset($_POST['username']) && isset($_POST["password"])) {
--- backend/login.php
+++ backend/login.php
@@ -1,5 +1,9 @@
 <?php
 
+header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Methods: GET, POST');
+header("Access-Control-Allow-Headers: X-Requested-With");
+
 include("connect.php");
 
 $response = [];
