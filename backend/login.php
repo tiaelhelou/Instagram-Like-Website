977391f244fedb59bff41b73f0fc481acb64ff29
tiahelou10 <tia.elhelou@outlook.com>
2022-11-20 18:55:25 +0200

    Backend: edited the login php

--- backend/login.php
+++ backend/login.php
@@ -2,32 +2,34 @@
 
 include("connect.php");
 
+if(isset($_POST['username']) && isset($_POST["password"]))
+{
+    $username = $_POST['username'];
+    $password = $_POST['password'];
 
-$username = $_POST['username'];
-$password = $_POST['password'];
+    $query = $mysqli -> prepare("SELECT id FROM users WHERE username = ? AND password = ?");
+    $query -> bind_param ("ss", $username, $password); 
+    $query -> execute();
 
-$query = $mysqli->prepare("SELECT id FROM users WHERE username = ? AND password = ?");
-$query->bind_param("ss", $username, $password);
-$query->execute();
+    $response = [];
 
-$response = [];
+    $result = $query -> get_result();
+    $id = $result->fetch_assoc();
+    $response[] = $id;
+    
+    if(mysqli_num_rows($result) > 0)
+    {
+        $_SESSION['username'] = $username;
 
-$result = $query->get_result();
-$id = $result->fetch_assoc();
-$response[] = $id;
+        echo json_encode($response);
+    }
+    else
+    {
+        $response["success"] = false;
 
-if (mysqli_num_rows($result) > 0) {
+        echo json_encode($response);
 
-    $_SESSION['id'] = $id;
-
-    echo json_encode($response);
-
-} 
-else {
-    $response["success"] = false;
-
-    echo json_encode($response);
-
+    }
 }
 
 ?>
